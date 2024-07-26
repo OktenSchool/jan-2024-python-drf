3d2ae4c26b1ba00e80ec3fec6079a1d16002ceee
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2024-07-26 19:41:03 +0200

    lesson2-hw

--- cars/filter.py
+++ cars/filter.py
@@ -3,6 +3,7 @@ from django.http import QueryDict
 from rest_framework.exceptions import ValidationError
 
 from cars.models import CarModel
+from cars.serializers import CarSerializer
 
 
 def car_filter(query: QueryDict) -> QuerySet:
@@ -13,6 +14,36 @@ def car_filter(query: QueryDict) -> QuerySet:
                 qs = qs.filter(price__gt=v)
             case 'price_lt':
                 qs = qs.filter(price__lt=v)
+            case 'price_gte':
+                qs = qs.filter(price__gte=v)
+            case 'price_lte':
+                qs = qs.filter(price__lte=v)
+
+            case 'year_gt':
+                qs = qs.filter(year__gt=v)
+            case 'year_lt':
+                qs = qs.filter(year__lt=v)
+            case 'year_gte':
+                qs = qs.filter(year__gte=v)
+            case 'year_lte':
+                qs = qs.filter(year__lte=v)
+
+            case 'brand_start':
+                qs = qs.filter(brand__istartswith=v)
+            case 'brand_end':
+                qs = qs.filter(brand__iendswith=v)
+            case 'brand_contains':
+                qs = qs.filter(brand__icontains=v)
+
+            case 'order':
+                fields = CarSerializer.Meta.fields
+                fields = [*fields, *[f'-{field}' for field in fields]]
+
+                if v not in fields:
+                    raise ValidationError({"details":f'Please choice order from {", ".join(fields)}'})
+
+                qs = qs.order_by(v)
+
             case _:
                 raise ValidationError(f"Filter {k} not supported")
     return qs
